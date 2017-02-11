fa90085045e22e3a71f7a41f8e4ca9eaa0dd2d5b
the_new_sky <lorisazerty@gmail.com>
2017-02-11 20:43:39 +0100

    Patch a small test mistake

--- kadot/tests/test_generators.py
+++ kadot/tests/test_generators.py
@@ -2,4 +2,6 @@ from kadot.generators import BaseGenerator
 
 def test_fit():
     generator = BaseGenerator()
-    assert generator.fit(['hello world !']).documents == ['START', 'hello ', 'world !', 'END']
+    generator.fit(['hello world !'])
+
+    assert generator.documents == ['START', 'hello ', 'world !', 'END']
