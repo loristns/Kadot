5ea5455c2b1b36b92b9686805214fba496d8b85e
the_new_sky <lorisazerty@gmail.com>
2017-02-12 16:43:10 +0100

    Add a test on generator (+ tokenizer)

--- kadot/tests/test_generators.py
+++ kadot/tests/test_generators.py
@@ -1,7 +1,16 @@
-from kadot.generators import BaseGenerator
+from kadot.generators import BaseGenerator, MarkovGenerator
+from kadot.tokenizers import SafeCharTokenizer
 
 def test_fit():
     generator = BaseGenerator()
     generator.fit(['hello world !'])
 
     assert generator.documents[0] == ['START', 'hello ', 'world !', 'END']
+
+
+def test_maxlen():
+    generator = MarkovGenerator()
+    generator.fit('a b a b a a b b b a b')
+    maxlen = 10
+
+    assert len(SafeCharTokenizer().tokenize(generator.predict(maxlen))) <= maxlen
