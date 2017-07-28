f614c4de73ddc711169f8c035a24e2b1d15cfb13
the_new_sky <lorisazerty@gmail.com>
2017-07-28 18:17:59 +0200

    BaseTokenizer.rebuilt_last() now generalised.

--- kadot/tokenizers.py
+++ kadot/tokenizers.py
@@ -6,18 +6,30 @@ DELIMITER_REGEX = "[.,!?:;()[\]{}><+\-*/\\= \"'\r\t\n\v\f@^¨`~_|]{1,}"
 
 
 class BaseTokenizer(object):
+
     def __init__(self):
         self.last_delimiters = []
         self.last_tokens = ""
+        self.last_starts_with_delimiter = False  # This attribute say if the first element is a delimiter or a token.
 
     def tokenize(self, text):
         pass
 
-    def rebuild_last(self):
+    def rebuild_last(self, tokens):
         """
         Rebuild a list of tokens using the delimiters of the last tokenized text.
         """
-        pass
+
+        if tokens is None:
+            tokens = self.last_delimiters
+
+        # Zip the two list starting by a delimiter or a token
+        if self.last_starts_with_delimiter:
+            zipped = itertools.zip_longest(self.last_delimiters, tokens)
+        else:
+            zipped = itertools.zip_longest(tokens, self.last_delimiters)
+
+        return ''.join([i for i in itertools.chain.from_iterable(zipped) if i])
 
 
 class SpaceTokenizer(BaseTokenizer):
@@ -31,12 +43,10 @@ class SpaceTokenizer(BaseTokenizer):
     """
 
     def tokenize(self, text):
+        self.last_starts_with_delimiter = text.startswith(" ")
         self.last_delimiters = [" " for _ in range(text.count(" "))]
         self.last_tokens = text.split(" ")
         return self.last_tokens
-
-    def rebuild_last(self):
-        return " ".join(self.last_tokens)
 
 
 class RegexTokenizer(BaseTokenizer):
@@ -53,7 +63,6 @@ class RegexTokenizer(BaseTokenizer):
         BaseTokenizer.__init__(self)
 
         self.delimiter = re.compile(delimiter)
-        self.last_starts_with_delimiter = False  # This attribute say if the first element is a delimiter or a token.
 
     def tokenize(self, text):
         self.last_starts_with_delimiter = self.delimiter.match(text) is not None
@@ -61,15 +70,3 @@ class RegexTokenizer(BaseTokenizer):
         self.last_tokens = [word for word in self.delimiter.split(text) if word]
 
         return self.last_tokens
-
-    def rebuild_last(self, tokens=None):
-        if tokens is None:
-            tokens = self.last_delimiters
-
-        # Zip the two list starting by a delimiter or a token
-        if self.last_starts_with_delimiter:
-            zipped = itertools.zip_longest(self.last_delimiters, tokens)
-        else:
-            zipped = itertools.zip_longest(tokens, self.last_delimiters)
-
-        return ''.join([i for i in itertools.chain.from_iterable(zipped) if i])
