e0c4e9fff91a4d959fec69b509be7e519290296e
the_new_sky <lorisazerty@gmail.com>
2018-06-22 10:49:07 +0200

    Add LSTMClassifier.

--- kadot/classifiers.py
+++ kadot/classifiers.py
@@ -198,6 +198,140 @@ class NeuralClassifier(SavedObject):
         return class_prediction
 
 
+class LSTMClassifier(SavedObject):
+    """
+    A word-embedding based LSTM classifier.
+    """
+
+    def __init__(self,
+                 train: Dict[str, str],
+                 word_vectors: Optional[VectorDict] = None,
+                 configuration: dict = NEURAL_CLASSIFIER_CONFIGURATION,
+                 tokenizer: Callable[..., Tokens] = regex_tokenizer,
+                 vectorizer_config: dict = DEFAULT_WORD2VEC_CONFIGURATION
+                 ):
+        """
+        :param train: a dictionary that contains training samples as keys
+         and their classes as values.
+
+        :param word_vectors: If provided, the VectorDict will be used as
+         word vectors.
+
+        :param configuration: a Dict that contains the parameters about
+         the model. It must contain these values :
+          - `hidden_size`
+          - `iter`
+          - `learning_rate`
+
+        :param tokenizer: the word tokenizer to use.
+
+        :param vectorizer_config: dictionary ("kwargs") giving the custom
+         parameters to `word2vec_vectorizer`.
+        """
+        import torch
+        from torch import nn, optim
+
+        # Initialize some variables
+        self.tokenizer = tokenizer
+        self.word_vectors = word_vectors
+
+        train_samples, train_labels = zip(*train.items())
+        train_tokens = corpus_tokenizer(train_samples, tokenizer=self.tokenizer)
+
+        if self.word_vectors is None:
+            from kadot.vectorizers import word2vec_vectorizer
+            self.word_vectors = word2vec_vectorizer(train_tokens,
+                                                    **vectorizer_config)
+
+        self.labels = unique_words(train_labels)
+        n_labels = len(self.labels)
+
+        class Network(nn.Module):
+            def __init__(self, embedding, hidden_size, output_size):
+                super(Network, self).__init__()
+                self.embedding = embedding
+                self.encoder = nn.LSTM(embedding.weight.shape[1], hidden_size)
+                self.classifier = nn.Linear(hidden_size, output_size)
+                self.softmax = nn.Softmax(dim=1)
+
+            def forward(self, inputs):
+                try:
+                    out = self.embedding(inputs)
+                except RuntimeError:
+                    out = torch.zeros(1, self.embedding.weight.shape[1])
+
+                out = out.view(out.shape[0], 1, out.shape[1]).float()
+                out, _ = self.encoder(out)
+                out = out[-1]
+                out = self.classifier(out)
+
+                return self.softmax(out)
+
+        # Initialize model
+        self.model = Network(
+            embedding=self.word_vectors.pytorch_embedding(),
+            hidden_size=configuration['hidden_size'],
+            output_size=n_labels
+        )
+        optimizer = optim.Adam(
+            [p for p in self.model.parameters() if p.requires_grad],
+            lr=configuration['learning_rate']
+        )
+        loss_fn = nn.NLLLoss()
+
+        # Training
+        logger.info("Starting model training.")
+        for epoch in range(configuration['iter']):
+            epoch_losses = []
+
+            for sentence, label in zip(train_tokens, train_labels):
+                x = []
+                for token in sentence:
+                    if token in self.word_vectors.keys():
+                        x.append(self.word_vectors.keys().index(token))
+
+                x = torch.tensor(x)
+                y = torch.tensor([self.labels.index(label)])
+
+                self.model.zero_grad()
+                y_pred = self.model(x)
+
+                loss = loss_fn(y_pred, y)
+                epoch_losses.append(float(loss))
+
+                loss.backward()
+                optimizer.step()
+
+            if epoch % round(configuration['iter'] / 10) == 0:
+                mean_loss = torch.tensor(epoch_losses).mean()
+                logger.info(
+                    "Epoch {} - Loss : {}".format(epoch, float(mean_loss))
+                )
+
+        logger.info("Model training finished.")
+
+    def predict(self, text: str) -> Dict[str, float]:
+        import torch
+
+        text = self.tokenizer(text)
+
+        with torch.no_grad():
+            x = []
+            for token in text:
+                if token in self.word_vectors.keys():
+                    x.append(self.word_vectors.keys().index(token))
+
+            x = torch.tensor(x)
+
+            prediction = self.model(x)
+
+        class_prediction = {}
+        for class_name, proba in zip(self.labels, prediction[0]):
+            class_prediction[class_name] = float(proba)
+
+        return class_prediction
+
+
 class FuzzyClassifier(SavedObject):
 
     def __init__(self,
